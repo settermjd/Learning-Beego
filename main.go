6c006477be78a6dcbf45f7bbb697f008a4fa10da
Matthew Setter <matthew@maltblue.com>
2014-05-16 10:03:33 +0200

    Added in ORM and database support
    Added the default SQLite database
    Added in a model
    Added in bootstrap support for the ORM and database

--- main.go
+++ main.go
@@ -1,11 +1,19 @@
 package main
 
 import (
-	_ "sitepointgoapp/routers"
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
+	_ "github.com/mattn/go-sqlite3"
+	models "sitepointgoapp/models"
+	_ "sitepointgoapp/routers"
 )
+
+func init() {
+	orm.RegisterDriver("sqlite", orm.DR_Sqlite)
+	orm.RegisterDataBase("default", "sqlite3", "database/orm_test.db")
+	orm.RegisterModel(new(models.Article))
+}
 
 func main() {
 	beego.Run()
 }
-
